511979f969502b786b25b8afc51a465e379d5ffe
edulazzari <eduardolazzari.el@gmail.com>
2024-11-27 21:01:03 -0300

    Update and rename lidar_plot3.py to lidar_plot.py

--- lidar_plot.py
+++ lidar_plot.py
@@ -5,14 +5,14 @@ import matplotlib.image as mpimg
 import re
 import os
 
-# Definição das variáveis de deslocamento
+# Define the offset variables.
 add_coord_x = 0
 add_coord_y = 280
 
 def carregar_dados(arquivo):
     with open(arquivo, 'r') as f:
         for _ in range(3):
-            next(f)  # Pula cabeçalho
+            next(f)  # Skip the header.
         dados = np.loadtxt(f)
     return dados[:, 0], dados[:, 1]
 
@@ -23,12 +23,12 @@ def plotar_dados():
 
     x = distancias * np.cos(angulos_radianos)
     y = distancias * np.sin(angulos_radianos)
-    x = -x  # Espelha os dados no eixo X
+    x = -x  # Mirror the data on the X-axis.
 
-    fov = 54  # Campo de Visão em graus
+    fov = 54  # Field of View in degrees. Adjust according to your camera.
     central_angle_deg = 90 + angulo_de_rotacao
 
-    angles_points = (np.degrees(np.arctan2(y, -x)) % 360)  # Normaliza ângulos
+    angles_points = (np.degrees(np.arctan2(y, -x)) % 360)  # Normalize angles
     angle_left_boundary = (central_angle_deg - fov / 2) % 360
     angle_right_boundary = (central_angle_deg + fov / 2) % 360
 
@@ -43,40 +43,40 @@ def rotacionar_pontos(points):
     def rotate(points, angles):
         angle_x, angle_y, angle_z = np.radians(angles)
        
-        # Matriz de rotação em torno do eixo X
+        # Rotation matrix around the X-axis.
         rot_x = np.array([[1, 0, 0],
                           [0, np.cos(angle_x), -np.sin(angle_x)],
                           [0, np.sin(angle_x), np.cos(angle_x)]])
        
-        # Matriz de rotação em torno do eixo Y
+        # Rotation matrix around the Y-axis.
         rot_y = np.array([[np.cos(angle_y), 0, np.sin(angle_y)],
                           [0, 1, 0],
                           [-np.sin(angle_y), 0, np.cos(angle_y)]])
        
-        # Matriz de rotação em torno do eixo Z
+        # Rotation matrix around the Z-axis.
         rot_z = np.array([[np.cos(angle_z), -np.sin(angle_z), 0],
                           [np.sin(angle_z), np.cos(angle_z), 0],
                           [0, 0, 1]])
        
-        # Rotaciona os pontos
+        # Rotate the points
         rotated_points = points @ rot_x.T @ rot_y.T @ rot_z.T
         return rotated_points
 
-    # Adiciona a dimensão Z aos pontos
+    # Add the Z dimension to the points.
     points_3d = np.hstack((points, np.zeros((len(points), 1))))
    
-    # Define os ângulos de rotação: 0 graus em X, 0 graus em Y e 0 graus em Z
+    # Set the rotation angles: 0 degrees for X, 0 degrees for Y, and 0 degrees for Z.
     rotated_points = rotate(points_3d, [0, 0, 0])
    
-    return rotated_points[:, :2]  # Retorna apenas as coordenadas x e y
+    return rotated_points[:, :2]  # Return only the x and y coordinates.
 
-# Carregar e processar os dados
+# Load and process the data.
 points_within_fov, angles_red_points = plotar_dados()
 
-# Remapeia os ângulos normalizados para coordenadas X (0 a 2592)
+# Remap the normalized angles to X coordinates (0 to 2592). Change according to the resolution of your camera.
 x_min = 0
 x_max = 2592
-y_line = 972  # Valor ajustável para a altura da linha
+y_line = 972  # Adjustable value for the line height.
 min_angle = np.min(angles_red_points)
 max_angle = np.max(angles_red_points)
 shift = -(min_angle + (max_angle - min_angle) / 2)
@@ -85,12 +85,12 @@ normalized_angles = angles_red_points + shift
 x_coords = np.interp(normalized_angles, (min(normalized_angles), max(normalized_angles)), (x_min, x_max))
 new_xy_coordinates = np.column_stack((x_coords, [y_line] * len(x_coords)))
 
-# Carregar e redimensionar a imagem
+# Load and resize the image.
 image_path = '/home/edu/Desktop/testando.jpg'
 image = np.rot90(mpimg.imread(image_path), k=0)
 resized_image = resize(image, (1944, 2592))
 
-# Filtrar os pontos que estão dentro do intervalo de x = 0 até x = 2592
+# Filter the points that are within the x range from 0 to 2592.
 filtered_xy_coordinates = []
 for point in new_xy_coordinates:
     shifted_x = point[0] + add_coord_x
@@ -98,7 +98,7 @@ for point in new_xy_coordinates:
         shifted_point = (shifted_x, point[1] + add_coord_y)
         filtered_xy_coordinates.append(shifted_point)
 
-# Obter valores RGB dos pixels
+# Get RGB values from the pixels.
 pixel_values = []
 for point in new_xy_coordinates:
     rounded_x = int(round(point[0] + add_coord_x))
@@ -108,96 +108,75 @@ for point in new_xy_coordinates:
         pixel_value = resized_image[adjusted_y, rounded_x] * 255
         pixel_values.append(pixel_value)
 
-# Corrigir a diferença de tamanhos entre arrays
+# Fix the size mismatch between arrays.
 points_within_fov_with_z = np.column_stack((points_within_fov, np.zeros(points_within_fov.shape[0])))
 pixel_values = np.array(pixel_values)  # Converta para array numpy
 
-# Certifique-se de que o número de pontos e valores RGB coincide
+# Make sure the number of points and RGB values match.
 min_len = min(len(points_within_fov_with_z), len(pixel_values))
 points_within_fov_with_z = points_within_fov_with_z[:min_len]
 pixel_values = pixel_values[:min_len]
 
-# Converta pixel_values para 2D, se necessário
+# Convert pixel_values to 2D, if necessary.
 pixel_values = pixel_values[:, np.newaxis] if pixel_values.ndim == 1 else pixel_values
 
-# Combine os dados
+# Combine the data
 combined_result = np.hstack((points_within_fov_with_z, pixel_values))
 
-# Função para obter o próximo nome de arquivo
+# Function to get the next file name.
 def get_next_filename(folder_path, base_name='plot_', extension='.txt'):
-    # Listar todos os arquivos no diretório
+    # List all the files in the directory.
     files = os.listdir(folder_path)
     
-    # Filtrar arquivos que correspondem ao padrão 'plot_x.txt'
+    # Filter files that match the pattern 'plot_x.txt'.
     plot_files = [f for f in files if f.startswith(base_name) and f.endswith(extension)]
     
-    # Se não houver arquivos existentes, comece com plot_1.txt
+    #If no files exist, start with plot_1.txt.
     if not plot_files:
         return os.path.join(folder_path, f"{base_name}1{extension}")
     
-    # Extrair o número do maior arquivo existente
+    # Extract the number from the highest existing file.
     numbers = [int(f[len(base_name):-len(extension)]) for f in plot_files]
     max_number = max(numbers)
     
-    # Retornar o próximo nome de arquivo
+    # Return the next file name.
     return os.path.join(folder_path, f"{base_name}{max_number + 1}{extension}")
 
-# Nome do próximo arquivo
-file_path = get_next_filename('/home/edu/plot/')
+# The name of the next file.
+file_path = get_next_filename('/home/user/plot/')
 
-# Caminho para o arquivo de texto do valor de inclinação do ângulo
-file_path_angle = '/home/edu/angle.txt'
+# Path to the text file containing the tilt angle value.
+file_path_angle = '/home/user/angle.txt'
 
-# Lê o conteúdo do arquivo
+# Read the content of the file.
 with open(file_path_angle, 'r') as file:
     content = file.read()
 
-# Usa expressão regular para extrair o valor numérico da inclinação, incluindo números negativos
+# Use a regular expression to extract the numerical tilt value, including negative numbers.
 angle_match = re.search(r'Average Inclination:\s*([-?\d.]+)°', content)
 if angle_match:
     angle_degrees = float(angle_match.group(1))
 else:
     raise ValueError("Could not find the angle in the text file.")
 
-# Converte o ângulo para radianos
+# Convert the angle to radians.
 angle_radians = np.deg2rad(angle_degrees)
 
-# Define a matriz de rotação para uma rotação em torno do eixo y
+# Define the rotation matrix for a rotation around the Y-axis.
 rotation_matrix_y = np.array([
     [np.cos(angle_radians), 0, np.sin(angle_radians)],
     [0, 1, 0],
     [-np.sin(angle_radians), 0, np.cos(angle_radians)]
 ])
 
-# Definir xyz
+# Define xyz
 xyz = combined_result[:, :3]
 
-# Rotacionar cada ponto
+# Rotate each point
 rotated_xyz = np.dot(xyz, rotation_matrix_y.T)
 
-# Salvar os dados rotacionados em um arquivo .txt
+# Save the rotated data in a .txt file.
 header = 'X Y Z R'
 np.savetxt(file_path, np.hstack((rotated_xyz, combined_result[:, 3:])), header=header, fmt='%10.5f')
 
 print(f"Arquivo salvo em: {file_path}")
-
-# Define limites estáticos dos eixos
-#x_limits = (0, 4000)
-#y_limits = (-2000, 1000)
-#z_limits = (2000, -2000)
-
-# Plotar os pontos originais e rotacionados
-#fig = plt.figure()
-#ax = fig.add_subplot(111, projection='3d')
-#ax.scatter(0, 0, 0, c='red', marker='o')
-#ax.scatter(rotated_xyz[:, 0], rotated_xyz[:, 1], rotated_xyz[:, 2], c=combined_result[:, 3:] / 255.0, edgecolors='none')
-#ax.set_xlabel('X')
-#ax.set_ylabel('Y')
-#ax.set_zlabel('Z')
-
-# Definir limites dos eixos
-#ax.set_xlim(x_limits)
-#ax.set_ylim(y_limits)
-#ax.set_zlim(z_limits)
-
-#plt.show()
